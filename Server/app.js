9c3cb14f42d6e81042b7e586ac1b4bc4d4b6fb84
Saavedra <aasaavedra@labs.neit.edu>
2018-03-01 14:43:13 -0500

    New files added 3/1

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,23 @@
 var io = require('socket.io')(process.envPort||3000);
 var shortid = require('shortid');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 
 console.log("Server Started");
 //console.log(shortid.generate());
 
 var players = [];
+
+var dbObj;
+MongoClient.connect(url, function(err, client){
+	if(err) throw err;
+	
+	dbObj = client.db('SocketGameData');
+});
+
+
+MongoClient.connect();
 
 io.on('connection', function(socket){
 	var thisPlayerId = shortid.generate();
@@ -32,5 +45,15 @@ io.on('connection', function(socket){
 		console.log("Player Disconnected");
 		players.splice(players.indexOf(thisPlayerId),1);
 		socket.broadcast.emit('disconnected', {id:thisPlayerId});
+	});
+	
+	socket.on('send data', function(data)
+	{
+		console.log(JSON.stringify(data));	
+		
+		dbObj.collection('playerData').save(data, function(err, results){
+			if(err) throw err;
+			console.log("Data saved to MongoDB");
+		});
 	});
 });
---
+++ SocketIOProject/SocketIOProject/Assets/GameDataEditor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using SocketIO;
+
+public class GameDataEditor : EditorWindow {
+    string gameDataFilePath = "/StreamingAssets/data.json";
+    public GameData editorData;
+    private GameObject server;
+    SocketIOComponent socket;
+
+    [MenuItem("Window/Game Data Editor")]
+    static void Init() {
+        EditorWindow.GetWindow(typeof(GameDataEditor)).Show();
+    }
+
+    void OnGUI(){ 
+        if (editorData != null){
+            // Display data from json
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = 
+                serializedObject.FindProperty("editorData");
+            EditorGUILayout.PropertyField(serializedProperty, true);
+            serializedObject.ApplyModifiedProperties();
+
+            if (GUILayout.Button("Save Game Data")){
+                SaveGameData();
+            }
+        }
+
+        if (GUILayout.Button("Load Game Data")) {
+            LoadGameData();
+        }
+
+        if (GUILayout.Button("Send Game Data"))
+        {
+            SendGameData();
+        }
+    }
+
+    void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataFilePath;
+
+        if (File.Exists(filePath))
+        {
+            string gameData = File.ReadAllText(filePath);
+            editorData = JsonUtility.FromJson<GameData>(gameData);
+        }
+        else {
+            editorData = new GameData();
+        }
+    }
+
+    void SaveGameData()
+    {
+        string jsonObj = JsonUtility.ToJson(editorData);
+        string filePath = Application.dataPath + gameDataFilePath;
+
+        File.WriteAllText(filePath, jsonObj);
+    }
+
+    void SendGameData()
+    {
+        string jsonObj = JsonUtility.ToJson(editorData);
+        server = GameObject.Find("server");
+        socket = server.GetComponent<SocketIOComponent>();
+
+        socket.Emit("send data", new JSONObject(jsonObj));
+    }
+}
